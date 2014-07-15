9f2cf1c84b1b72b9c955efb09e8218a9be13cbd7
Bardia Khosravi <bardia.khosravi@gmail.com>
2014-07-15 08:13:56 -0400

    AngularJS + RequireJS seed

---
+++ www/js/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by bkhosravi on 2014-04-01.
+ */
+define(['angular', 'controller/HomeController'],
+
+    function(angular, HomeController) { 
+
+        var app = angular.module('mainApp', []);
+        
+        app.controller("HomeController", HomeController);
+
+        return app;
+    }
+    
+);
